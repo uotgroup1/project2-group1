4d2402dece9c1df2b824ef10f30dde2df8d24955
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-07-24 15:47:00 -0400

    fix the server

--- server.js
+++ server.js
@@ -4,9 +4,29 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set Handlebars as the default template engine.
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//------------------------------------------------------------------
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//-------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });

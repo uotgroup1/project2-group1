6d6d3a33380de7398e090d97f60d7ace7cfe9af4
Mayuran <mayuranswee23@gmail.com>
2021-07-24 15:47:59 -0400

    added modules for handlebars and added template engine to server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const express = require('express');
-const sequelize = require('./config/connection')
+const sequelize = require('./config/connection');
+const exphbs = require ('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));

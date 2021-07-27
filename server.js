c46fdcd4042e463333419e87b3f3aa4ac34ddcbe
Zohaib Ali <s.zohaibali3@gmail.com>
2021-07-27 17:18:07 -0400

    fixed routes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const express = require('express');
+
 const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection')
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;

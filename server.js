9d86ad1852716fda76a70c94fb860881281116e4
Zohaib Ali <s.zohaibali3@gmail.com>
2021-07-27 17:53:41 -0400

    fixed views

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const express = require('express');
-
 const exphbs = require('express-handlebars');
-const sequelize = require('./config/connection')
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controller'));
+app.use(require('./controller/'));
 //------------------------------------------------------------------
 
 const session = require('express-session');

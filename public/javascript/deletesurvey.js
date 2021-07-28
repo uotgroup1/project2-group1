1edb2b1310e67e1e2a381fc2783e4f10af03695b
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-07-28 15:47:11 -0400

    delete survey

--- public/javascript/deletesurvey.js
+++ public/javascript/deletesurvey.js
@@ -1,17 +1,21 @@
 //delete survey fxn
-async function deleteSurveyHandler (event) {
-event.preventDefault()
-//need to define a constant which identifies which survey to delete
-const id = [] //once we set up the id we can traverse through the DOM to get the ID?
-const response = await fetch(`api/surveys/delete/${id}`, {//which survey we want to delete
-method:'post'
-})
-if (response.ok) {
-    document.location.replace('/userDashboard')//if response is ok take them back to their own dashboard
-}
-else {
-    alert(response.statusText)
-}
+async function deleteSurveyHandler(event) {
+  event.preventDefault();
+  //need to define a constant which identifies which survey to delete
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ]; //once we set up the id we can traverse through the DOM to get the ID?
+  const response = await fetch(`api/surveys/${id}`, {
+    //which survey we want to delete
+    method: 'DELETE',
+  });
+  if (response.ok) {
+    document.location.replace('/userDashboard'); //if response is ok take them back to their own dashboard
+  } else {
+    alert(response.statusText);
+  }
 }
 
-document.querySelector('#delete-btn').addEventListener('click', deleteSurveyHandler)
+document
+  .querySelector('#delete-btn')
+  .addEventListener('click', deleteSurveyHandler);
